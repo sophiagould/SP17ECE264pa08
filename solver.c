ef18aade1cfa9589935221c223a05ab6d597d65c
Sophia Alexandra Gould <gould7@ecegrid-thin1.ecn.purdue.edu>
2017-03-03 20:03:50 -0500

    first

--- solver.c
+++ solver.c
@@ -20,6 +20,46 @@ char * solveMaze(Maze * m) {
 bool depthFirstSolve(Maze * m, MazePos curpos, char * path, int step) {
 
 	//FILL IN
+	if(curpos.ypos < 0 || curpos.xpos < 0 || curpos.xpos >= m -> width || curpos.ypos >= m -> height){
+		return false;
+	}
+	if(m -> maze[curpos.ypos][curpos.xpos].type == WALL){
+		return false;
+	}
+	if(m -> maze[curpos.ypos][curpos.xpos].type == END){
+		path[step] = '\0';
+		return true;
+	}
+	if(m -> maze[curpos.ypos][curpos.xpos].visited == true){
+		return false;
+	}
 
-	return false;
+	m -> maze[curpos.ypos][curpos.xpos].visited = true;
+	curpos.xpos--;
+	if(depthFirstSolve(m,curpos,path,step +1) == true){
+		path[step] = WEST;
+		return true;
+	}
+	curpos.xpos++;
+	curpos.xpos++;
+	
+	if(depthFirstSolve(m,curpos,path,step +1) == true){
+		path[step] = EAST;
+		return true;
+	}
+	curpos.xpos--;
+	curpos.ypos--;
+
+	if(depthFirstSolve(m,curpos,path,step +1) == true){
+		path[step] = NORTH;
+		return true;
+	}
+	curpos.ypos++;
+	curpos.ypos++;
+	
+	if(depthFirstSolve(m,curpos,path,step +1) == true){
+		path[step] = SOUTH;
+		return true;
+	}
+	curpos.ypos--;
 }
